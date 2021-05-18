cb99b4fc3b3cf6c3b95932c93013890d9cf0727e
Marlo Baguyos <marlo.baguyos@outlook.com>
2021-05-17 20:43:50 -0400

    update

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # 3rd party
+    'cripy_forms', 
+
+    # Local
     'accounts',
     'pages'
 ]
